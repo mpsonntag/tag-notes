a0412f29a944e0c14cc032dad76a4a957320b02c
M. Sonntag <michael.p.sonntag@gmail.com>
2015-11-12 21:08:15 +0100

    setUpRepo: add first class

---
+++ src/App.kt
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2015, Michael Sonntag
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ */
+
+fun main(args: Array<String>) {
+    println("First line ever! (Yay \\o/)")
+}
