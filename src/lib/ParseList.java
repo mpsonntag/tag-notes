561f62f114b64e15b12001967bc281eb96d47d1e
M. Sonntag <michael.p.sonntag@gmail.com>
2016-02-01 15:59:41 +0100

    parse text to entity

--- src/lib/ParseList.java
+++ src/lib/ParseList.java
@@ -23,24 +23,48 @@ public final class ParseList {
         List<TaggedEntity> retList = new ArrayList<>();
 
         System.out.println(
-                String.join("", "List has a length of: ",
+                String.join("", "[DEBUG] List has a length of: ",
                         Integer.toString(toParse.size()), "\n")
         );
 
         for (String currLine : toParse) {
             if (currLine.contains("(#)") & currLine.endsWith("#)")) {
-                System.out.println("TagLine");
-                System.out.println(currLine);
+
+                System.out.println("[DEBUG] TagLine");
 
                 retList.add(new TaggedEntity());
+
+                String[] ls = currLine
+                        .substring(currLine.indexOf("(#)")+"(#)".length(),
+                                currLine.length()-"#)".length())
+                        .split(",");
+
+                for (String s : ls) {
+                    retList.get(retList.size()-1).getTags().add(s);
+                }
+
+            } else if (!retList.isEmpty())  {
+                System.out.println("[DEBUG] BodyLine");
+
+                String currString = retList.get(retList.size()-1).getBody();
+                retList.get(retList.size()-1).setBody(
+                        String.join("", currString, currLine, "\n")
+                );
             } else {
-                System.out.println("BodyLine");
-                System.out.println(currLine);
+                System.out.println("[WARNING] Found line not connected to a tag, will be omitted.");
             }
         }
 
-        System.out.println(String.join("", "# of Tagged entities: ",
+        System.out.println(String.join("", "[DEBUG] # of Tagged entities: ",
                 Integer.toString(retList.size()))
+        );
+
+        retList.forEach(c -> System.out.println(
+                String.join("", "[DEBUG] ", "ID: ", c.getEntityID(), " Tags: ",
+                        Integer.toString(c.getTags().size()), " ", c.getTags().toString(),
+                        " Body: ", c.getBody()
+                    )
+                )
         );
 
         return retList;
