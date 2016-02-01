47fcd4cc31117e4bc5b59832ffe5fec38a92abd7
M. Sonntag <michael.p.sonntag@gmail.com>
2016-02-01 17:17:09 +0100

    add java package info files

---
+++ src/main/java/org/mps/lib/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2016, Michael Sonntag
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ *
+ * Provides all library methods to open files, parse their content and save
+ * it to a graph database.
+ */
+
+package org.mps.lib;
---
+++ src/main/java/org/mps/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2016, Michael Sonntag
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ *
+ * Main application to tag notes and write the corresponding information to a graph database.
+ */
+
+package org.mps;
