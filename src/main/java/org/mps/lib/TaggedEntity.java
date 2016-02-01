3e3ba053a5cd2554feade5cc35244f4a450789c8
M. Sonntag <michael.p.sonntag@gmail.com>
2016-02-01 17:34:58 +0100

    cleanup TaggedEntity

--- src/main/java/org/mps/lib/TaggedEntity.java
+++ src/main/java/org/mps/lib/TaggedEntity.java
@@ -15,44 +15,65 @@ import java.util.List;
 import java.util.UUID;
 
 /**
+ * Prototype class.
  * Instances contain all information of a tagged entity.
  */
-public class TaggedEntity {
-
+public final class TaggedEntity {
+    /**
+     * String to identify the entity. Default constructor creates a new UUID as entityID.
+     */
     private String entityID;
+    /**
+     * Tags associated with the tagged entity.
+     */
     private List<String> tags;
+    /**
+     * Main content of the tagged entity.
+     */
     private String body;
-
+    /**
+     * Default constructor of the TaggedEntity.
+     */
     TaggedEntity() {
         this.entityID = UUID.randomUUID().toString();
         this.tags = new ArrayList<>();
         this.body = "";
     }
-
-    TaggedEntity(String entityID) {
-        this.entityID = entityID;
+    /**
+     * Constructor of a TaggedEntity, initializing it with a given ID.
+     * @param inEntityID ID used to initialize the ID attribute of the instance.
+     */
+    TaggedEntity(final String inEntityID) {
+        this.entityID = inEntityID;
         this.tags = new ArrayList<>();
         this.body = "";
     }
-
-
+    /**
+     * Get the ID String associated with this tagged entity.
+     * @return See description.
+     */
     public String getEntityID() {
-        return entityID;
+        return this.entityID;
     }
-
+    /**
+     * Get all tags assigned to this tagged entity.
+     * @return List of Strings corresponding to the tags associated with this entity.
+     */
     public List<String> getTags() {
-        return tags;
+        return this.tags;
     }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
+    /**
+     * Get the main text of the tagged entity.
+     * @return See description.
+     */
     public String getBody() {
-        return body;
+        return this.body;
     }
-
-    public void setBody(String body) {
-        this.body = body;
+    /**
+     * Set the main text of the tagged entity.
+     * @param inBody String containing the current main text of the tagged entity.
+     */
+    public void setBody(final String inBody) {
+        this.body = inBody;
     }
 }
